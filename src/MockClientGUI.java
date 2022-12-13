d7f0a9207975c0f7c7f99506d35ab698ab35e044
idacjohansson <johansson.c.ida@gmail.com>
2022-12-13 14:11:11 +0100

    Initial commit

---
+++ src/MockClientGUI.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+public class MockClientGUI {
+    private JPanel Panel;
+    private JLabel WelcomeLabel;
+    private JPanel Panel2;
+    private JButton butik;
+    private JButton lager;
+    private JLabel Text;
+    private JButton inköp;
+
+    public MockClientGUI() {
+        butik.addActionListener(e -> {
+            if (butik == e.getSource()){
+
+            }
+
+
+        });
+        lager.addActionListener(e -> {
+
+
+        });
+        inköp.addActionListener(e -> {
+
+
+        });
+    }
+}
---
+++ src/MockServer.java
@@ -0,0 +1,7 @@
+public class MockServer {
+}
+/*
+ger access till olika funktioner beroende på vilken aktör det är som arbetar från Client
+
+
+ */
