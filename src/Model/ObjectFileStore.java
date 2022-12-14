483549cbd6d91df659fb713fbc4d03cbf55c63c8
Lukas Chyle <lukas_chyle@hotmail.com>
2022-12-14 22:52:32 +0100

    resource save folder for ObjectFileStore

--- src/Model/ObjectFileStore.java
+++ src/Model/ObjectFileStore.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class ObjectFileStore {
 
     public static void storeObjectList(List<?> objectList, String fileName){
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName.trim() + ".ser", true))) {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(
+                "resources/files/" + fileName.trim() + ".ser", true))) {
             out.writeObject(objectList);
         } catch (IOException e) {
             e.printStackTrace();
@@ -14,7 +15,8 @@ public class ObjectFileStore {
     }
 
     public static List<?> retrieveObjectList(String fileName){
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName.trim() + ".ser"))) {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(
+                "resources/files/" + fileName.trim() + ".ser"))) {
             return (List<?>) in.readObject();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
