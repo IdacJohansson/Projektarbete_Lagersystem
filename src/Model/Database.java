5f88fb43f8d4da651aa9385195fcfd04a0c2e656
Lukas Chyle <lukas_chyle@hotmail.com>
2022-12-14 18:22:12 +0100

    Store and retrieve system for List with object

--- src/Model/Database.java
+++ src/Model/Database.java
@@ -1,4 +1,32 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Database {
+
+    private List<Article> articles;
+
+    // create a constructor that calls retrieveList().
+
+    // call storeList() in the end of every method that makes changes to the list or its objects.
+
+
+
+
+
+
+    private void storeList() {
+        ObjectFileStore.storeObjectList(articles, "articles");
+    }
+
+    @SuppressWarnings("unchecked")
+    private void retrieveList() {
+        List<Article> articleList = (List<Article>) ObjectFileStore.retrieveObjectList("articles");
+        if (articleList != null) {
+            articles = articleList;
+        } else {
+            articles = new ArrayList<>();
+        }
+    }
 }
---
+++ src/Model/ObjectFileStore.java
@@ -0,0 +1,24 @@
+package Model;
+
+import java.io.*;
+import java.util.List;
+
+public class ObjectFileStore {
+
+    public static void storeObjectList(List<?> objectList, String fileName){
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName.trim() + ".ser", true))) {
+            out.writeObject(objectList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<?> retrieveObjectList(String fileName){
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName.trim() + ".ser"))) {
+            return (List<?>) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
