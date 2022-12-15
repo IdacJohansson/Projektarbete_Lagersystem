dd92d96c00f2a094d12995113cb484ce91f14600
Lukas Chyle <lukas_chyle@hotmail.com>
2022-12-15 13:09:27 +0100

    newArticle GUI, empty controller

---
+++ src/Controller/NewArticle.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import javax.swing.*;
+
+public class NewArticle {
+    private JTextField articleNrField;
+    private JComboBox garment;
+    private JComboBox size;
+    private JComboBox color;
+    private JTextField balanceField;
+}
