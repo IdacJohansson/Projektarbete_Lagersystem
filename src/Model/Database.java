8c7fbe2fb272970420cfe4574024e2c34ba32ea8
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2022-12-16 13:04:17 +0100

    kopplat ihop Database med ArticleMaker

--- src/Model/Database.java
+++ src/Model/Database.java
@@ -5,24 +5,27 @@ import java.util.List;
 
 public class Database {
 
-    protected List<Article> articles;
+    protected List<Article> allArticles;
 
     protected List<Article> oneCategory;
+    private ArticleMaker articleMaker;
 
 
     public Database() {
-        articles = new ArrayList<>();
+        allArticles = new ArrayList<>();
         oneCategory = new ArrayList<>();
-        setListOfArtNr();
+
+        articleMaker = new ArticleMaker(allArticles);
+        articleMaker.setArticlesList();
     }
 
 
     public List<Article> getListOfArtNr() {
-        return articles;
+        return allArticles;
     }
 
     public List<Article> getCategory(Garment garment) {
-        for (Article a : articles) {
+        for (Article a : allArticles) {
             if (a.getGarment().equals(garment)) {
                 oneCategory.add(a);
             }
@@ -36,43 +39,9 @@ public class Database {
     // call storeList() in the end of every method that makes changes to the list or its objects.
 
 
-    public void setListOfArtNr() {
-        Article tXS = new Article("500 06", Garment.SWEATER, Color.BLACK, Size.XS);
-        Article tS = new Article("500 08", Garment.SWEATER, Color.BLACK, Size.S);
-        Article tM = new Article("500 10", Garment.SWEATER, Color.BLACK, Size.M);
-        Article tL = new Article("500 12", Garment.SWEATER, Color.BLACK, Size.L);
-
-        Article bXS = new Article("601 06", Garment.TROUSER, Color.WHITE, Size.XS);
-        Article bS = new Article("601 08", Garment.TROUSER, Color.WHITE, Size.S);
-        Article bM = new Article("601 10", Garment.TROUSER, Color.WHITE, Size.M);
-        Article bL = new Article("601 12", Garment.TROUSER, Color.WHITE, Size.L);
-
-        Article tsXS = new Article("701 06", Garment.T_SHIRT, Color.WHITE, Size.XS);
-        Article tsS = new Article("701 08", Garment.T_SHIRT, Color.WHITE, Size.S);
-        Article tsM = new Article("701 10", Garment.T_SHIRT, Color.WHITE, Size.M);
-        Article tsL = new Article("701 12", Garment.T_SHIRT, Color.WHITE, Size.L);
-
-        articles.add(tXS);
-        articles.add(tS);
-        articles.add(tM);
-        articles.add(tL);
-
-        articles.add(bXS);
-        articles.add(bS);
-        articles.add(bM);
-        articles.add(bL);
-
-        articles.add(tsXS);
-        articles.add(tsS);
-        articles.add(tsM);
-        articles.add(tsL);
-
-        //Collections.sort();
-    }
-
-    public Article getArticle(String artNumb) {
-        for (Article a : articles) {
-            if (a.getArticleNumber().equals(artNumb)) {
+    public Article getArticle(String artNr) {
+        for (Article a : allArticles) {
+            if (a.getArticleNumber().equals(artNr)) {
                 return a;
             }
 
