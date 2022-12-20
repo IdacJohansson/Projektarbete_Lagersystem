817fe433ab74a4cae8d7ba29d8b801fc726e12fa
Lukas Chyle <lukas_chyle@hotmail.com>
2022-12-20 17:07:46 +0100

    Implemented an interface

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,13 +1,18 @@
 package Controller;
 
+import Model.StartClient;
+
 import javax.swing.*;
 
-public class Main {
+public class Main implements StartClient {
 
     public static void main(String[] args) {
+        Main main = new Main();
+        main.startClient();
+    }
 
+    @Override
+    public void startClient() {
         JFrame mockGUI = new MockClient();
-
-
     }
 }
